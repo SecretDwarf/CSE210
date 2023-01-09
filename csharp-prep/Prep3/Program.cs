da5b261700c6391af4f4fb55bd0802fb4d82f794
Dopeycylender39 <briggsjjacob@gmail.com>
2023-01-09 09:58:16 -0700

    prep3 commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        bool guessed = false;
+        int turns = 0;
+        while (guessed == false)
+        {
+        turns += 1;
+        Console.Write("Guess the magic number between 1 and 100. ");
+        int answer = int.Parse(Console.ReadLine());
+        if (answer > number)
+        {
+            Console.WriteLine("Guess Lower");
+        }
+        else if (answer < number)
+        {
+            Console.WriteLine("Guess Higher");
+        }
+        else
+        {
+            Console.WriteLine("Correct. Perfect. Great Job. You win!");
+            Console.WriteLine($"You guessed {answer} after {turns} times.");
+            guessed = true;
+        }
+        }
     }
 }
